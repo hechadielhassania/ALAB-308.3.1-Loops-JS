52c3695422886e38bfee0e167c20672b16228af2
Hechadi El Hassania <hechadihassaia@gmail.com>
2024-03-15 11:25:31 -0400

    Part 3: Feeling Loopy

--- loop.js
+++ loop.js
@@ -42,3 +42,28 @@ function findNextPrime(n) {
 let n = 4; // Arbitrary number
 console.log("Next prime number after", n, "is:", findNextPrime(n));
 
+//---------------------------------//
+console.log("-----------------------------")
+//---------------------------------//
+
+//Part 3: Feeling Loopy
+
+const csvData = `ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26`;
+
+let currentCell = "";
+let rowData = [];
+for (let i = 0; i < csvData.length; i++) {
+    const char = csvData[i];
+    if (char === ",") {
+        rowData.push(currentCell);
+        currentCell = "";
+    } else if (char === "\n") {
+        rowData.push(currentCell);
+        console.log(...rowData);
+        currentCell = "";
+        rowData = [];
+    } else {
+        currentCell += char;
+    }
+}
+
