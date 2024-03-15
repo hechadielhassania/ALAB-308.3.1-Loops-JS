8da6ccd29b0464acc2a57371bd57c9268b4d5857
Hechadi El Hassania <hechadihassaia@gmail.com>
2024-03-15 11:22:39 -0400

    Part 2: Prime Time

--- loop.js
+++ loop.js
@@ -12,3 +12,33 @@ for (let i = 1; i <= 100; i++) {
     }
 }
 
+//---------------------------------//
+console.log("-----------------------------")
+//---------------------------------//
+
+//Part 2: Prime Time
+
+function isPrime(num) {
+    if (num <= 1) {
+        return false;
+    }
+    for (let i = 2; i * i <= num; i++) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function findNextPrime(n) {
+    while (true) {
+        if (isPrime(n)) {
+            return n;
+        }
+        n++;
+    }
+}
+
+let n = 4; // Arbitrary number
+console.log("Next prime number after", n, "is:", findNextPrime(n));
+
